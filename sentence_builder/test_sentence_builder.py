7daf6da4e48fb896220f8e862c6417ccc9d695cb
Michael Alburquerque <michaelalb@gmail.com>
2022-03-27 17:48:13 +0300

    Works 1. maybe bigger corpus 2. maybe bigger vocab

--- sentence_builder/test_sentence_builder.py
+++ sentence_builder/test_sentence_builder.py
@@ -6,11 +6,14 @@ import pandas as pd
 from textsplit.tools import get_penalty, get_segments
 from textsplit.algorithm import split_optimal, split_greedy, get_total
 from pathlib import Path
-
+import word2vec
 from sentence_builder.segmentation_metrics import get_casas_data, break_seq_p_k, break_seq_wd
+from textsplit.tools import SimpleSentenceTokenizer
+from sklearn.feature_extraction.text import CountVectorizer
+from gensim.models import Word2Vec
 
 
-class SentenceBuilder(unittest.TestCase):
+class SentenceBuilder():
     """Sentence segmentation on generated sentences"""
 
     def test_casas(self):
@@ -26,16 +29,28 @@ class SentenceBuilder(unittest.TestCase):
         casas_folder_path = Path(os.path.dirname(__file__)) / 'data' / 'adlnormal'
         true_sent_breaks, casas_df = get_casas_data(casas_folder_path)
 
-        sentence_vectors = casas_df['Description_ID']
-        sentence_vectors = sentence_vectors.to_numpy().reshape(-1, 1)
-        # todo: why is the penalty always zero
-        target_seg_len_in_sent = 50
-        # penalty = get_penalty(sentence_vectors, target_seg_len_in_sent)
-        penalty = 0.2
-        optimal_segmentation = split_optimal(sentence_vectors, penalty, seg_limit=target_seg_len_in_sent)
 
-        # model = NGramModel(casas_df, self.export_path, self.export_path)
-        # sent_break_index = model.run(verbose=0)
+        # original word2vec
+        wrdvec_path = 'wrdvecs.bin'
+        if not os.path.exists(wrdvec_path):
+            word2vec.word2vec("C:/Users/Lenovo/PycharmProjects/Action-Segmentator/sentence_builder/data/text_with_spaces.txt", wrdvec_path, cbow=1, iter_=5, hs=1, threads=8, sample='1e-5', window=15,
+                              size=200, binary=1)
+
+        model = word2vec.load(wrdvec_path)
+        wrdvecs = pd.DataFrame(model.vectors, index=model.vocab)
+
+        a = []
+        for i in casas_df['Description_ID'].values:
+            if str(i) not in wrdvecs.index:
+                print(i)
+                continue
+            a.append(pd.DataFrame(wrdvecs.loc[str(i)]).T)
+        df = pd.concat(a)
+
+        word_context_vectors = df.to_numpy().dot(wrdvecs.T)
+        target_seg_len_in_sent = 45
+        penalty = get_penalty([word_context_vectors], target_seg_len_in_sent)
+        optimal_segmentation = split_optimal(word_context_vectors, penalty, seg_limit=250)
 
         predicted_sentences = sorted(optimal_segmentation.splits) + [len(casas_df)]
         true_sentences = sorted(true_sent_breaks)
